8b91906cdb540f36af84bd671cf19b5424d00590
OssGoku <fafetnach@gmail.com>
2016-09-08 14:38:27 +0200

    mvc ok

--- controller/traitement_producer.php
+++ controller/traitement_producer.php
@@ -1,1 +1,27 @@
-traitement_producer.php
+<?php
+	var_dump($_POST);
+
+	if(isset($_POST["register"], $_POST['society'], $_POST['siret'], $_POST['mail'], $_POST['password'], $_POST['password2'], $_POST['address'], $_POST['phone'])){
+
+		$manager = new ProducerManager($db);
+		// $list = $manager->create($password, $mail, $name, $firstname, $address, $phone, $admin);
+		$list = $manager->create($_POST['society'], $_POST['siret'], $_POST['mail'], $_POST['password'], $_POST['password2'], $_POST['address'], $_POST['phone'], $admin);
+		header("Location: index.php?page=login");
+		exit;
+	}
+
+	if(isset($_POST["login"], $_POST['society'], $_POST['password'])){
+
+		
+		if(!empty($_POST['society'] && !empty($_POST['password']))){
+			$manager = new UserManager($db);
+			$list = $manager->findAll();
+			header("Location: index.php?page=login");
+			exit;
+		}
+		else{
+			$error = "Veuillez remplir tout les champs";
+			var_dump($error);
+		}
+	}
+?>
--- controller/traitement_user.php
+++ controller/traitement_user.php
@@ -1,4 +1,28 @@
 <?php
-	$manager = new UserManager($db);
-	$list = $manager->create();
+	var_dump($_POST);
+
+	if(isset($_POST["register"], $_POST['password'], $_POST['mail'], $_POST['name'], $_POST['firstname'], $_POST['address'], $_POST['phone'])){
+		$admin = ";";
+
+		$manager = new UserManager($db);
+		// $list = $manager->create($password, $mail, $name, $firstname, $address, $phone, $admin);
+		$list = $manager->create($_POST['password'], $_POST['mail'], $_POST['name'], $_POST['firstname'], $_POST['address'], $_POST['phone'], $admin);
+		header("Location: index.php?page=login");
+		exit;
+	}
+
+	if(isset($_POST["login"], $_POST['name'], $_POST['password'])){
+
+		
+		if(!empty($_POST['name'] && !empty($_POST['password']))){
+			$manager = new UserManager($db);
+			$list = $manager->findAll();
+			header("Location: index.php?page=login");
+			exit;
+		}
+		else{
+			$error = "Veuillez remplir tout les champs";
+			var_dump($error);
+		}
+	}
 ?>
