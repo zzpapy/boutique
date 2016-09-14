caa686490d25a7720ebdbd6a5fc49493fae4ce5c
OssGoku <fafetnach@gmail.com>
2016-09-14 11:38:36 +0200

    login pro ok

--- controller/header.php
+++ controller/header.php
@@ -16,21 +16,3 @@
 		require("view/header.phtml");
 	}
 ?>
-<!-- 
-
-	if(isset($_SESSION["user"], $_SESSION['admin']))
-	{
-		if($_SESSION['admin'] == 0){
-			require("view/header_in_user.phtml");
-		}
-		else if($_SESSION['admin'] == 1){
-			require("view/header_in_admin.phtml");
-		}
-		else if($_SESSION['admin'] == 2){
-			require("view/header_in_pro.phtml");
-		}
-	}
-	else
-	{
-		require("view/header.phtml");
-	} -->
